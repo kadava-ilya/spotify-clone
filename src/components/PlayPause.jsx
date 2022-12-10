3db659686b3123106a89dfcbf0e65a53e0dbf340
Ilya.Kadava <kadavaisback@gmail.com>
2022-12-10 23:27:46 +0000

    finished track card, sidebar and player

---
+++ src/components/PlayPause.jsx
@@ -0,0 +1,10 @@
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
+
+const PlayPause = ({ song, isPlaying, activeSong, handlePause, handlePlay }) => (
+  (isPlaying && activeSong?.title === song.title)
+    ? <FaPauseCircle className='text-gray-300' size={35} onClick={handlePause} />
+    : <FaPlayCircle className='text-gray-300' size={35} onClick={handlePlay} />
+);
+
+export default PlayPause;
+
